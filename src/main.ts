9bfb31bf96c0cbde0aae5cd200ad65ce1f5af89f
Binteak <binteak@gmail.com>
2025-08-05 18:49:20 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [
+  providers: [ 
     provideRouter(appRoutes), 
     importProvidersFrom(BrowserAnimationsModule),
     importProvidersFrom(HttpClientModule)
