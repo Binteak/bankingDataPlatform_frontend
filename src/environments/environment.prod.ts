3b53025fef9c486801d962515811257690fbc87b
Binteak <binteak@gmail.com>
2025-08-05 18:56:02 +0200

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://pruebaappbackend.onrender.com/api'
-    apiURL: process.env['API_URL'] || ''
+    apiUrl: 'https://pruebaappbackend.onrender.com/api'
+    // apiURL: process.env['API_URL'] || ''
   };
