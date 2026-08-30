8f5d7a7e9878773c69a658afce4e860b1388dfe4
Binteak <binteak@gmail.com>
2026-08-30 12:55:02 +0200

    Actualiza configuración tests, dependencias y tipado del proyecto

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,16 @@
 // Karma configuration file, see link for more information
+
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    frameworks: [
+      'jasmine',
+      '@angular-devkit/build-angular'
+    ],
+
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -12,27 +18,41 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        // Jasmine configuration
       },
     },
+
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true
     },
+
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontend'),
+      dir: require('path').join(
+        __dirname,
+        './coverage/frontend'
+      ),
+
       subdir: '.',
+
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
-    browsers: ['Chrome'],
+
+    reporters: [
+      'progress',
+      'kjhtml'
+    ],
+
+    browsers: [
+      'Chrome'
+    ],
+
     restartOnFileChange: true
   });
 };
